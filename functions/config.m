c0a126ff425024ba07b5bc55e1e9c0d6a5dceea9
matthewq22 <matthew@quinn.id.au>
2026-08-16 16:34:20 +0800

    Checks tht RTL-SDR is connected on calibration

--- functions/config.m
+++ functions/config.m
@@ -1,9 +1,16 @@
-function config(app)
+function sdrCon = config(app)
     app.centreFreq = app.CentreFrequencySlider.Value * 10^6;
     app.fftSize = str2double(app.FFTSizeDropDown.Value);
     app.window = hann(app.fftSize);
-    app.sdr = comm.SDRRTLReceiver(CenterFrequency=app.centreFreq, SampleRate=app.sampleRate, OutputDataType='double', SamplesPerFrame=app.fftSize);
     rawFreqs = ((-app.fftSize/2 : app.fftSize/2 - 1) * (app.sampleRate / app.fftSize)); 
     app.freq = (app.centreFreq + rawFreqs) / 1e6; % MHz
     app.plotLine = min(app.freq) < app.targetFreq && max(app.freq) > app.targetFreq;
+    
+    sdrCon = false;
+    app.sdr = comm.SDRRTLReceiver(CenterFrequency=app.centreFreq, SampleRate=app.sampleRate, OutputDataType='double', SamplesPerFrame=app.fftSize);
+    if app.sdr.info.RadioName ~= "Cannot find radio"
+        sdrCon = true;
+    else
+        rtlNotConnected(app);
+    end
 end
